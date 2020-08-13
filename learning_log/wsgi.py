bdcbc8282c81329cd130189df855c4655af7ac28
pythonista-SLY <malonekeller@gmail.com>
2020-08-13 17:56:28 +0000

    Removed application fro wsgi.py'

--- learning_log/wsgi.py
+++ learning_log/wsgi.py
@@ -15,4 +15,3 @@ from dj_static import Cling
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'learning_log.settings')
 application = Cling(get_wsgi-application())
 
-application = get_wsgi_application()
